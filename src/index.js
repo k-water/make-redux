4fdf232509d06ee41098b55d978fc87d8213fd16
k-water <625592890@qq.com>
2017-11-26 23:52:59 +0800

    fix：optimize render

--- src/index.js
+++ src/index.js
@@ -1,29 +1,15 @@
-const appState = {
-  title: {
-    text: 'water make redux',
-    color: 'red'
-  },
-  content: {
-    text: 'water make redux',
-    color: 'green'
-  }
-}
-
-function renderApp(appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
-}
 /**
  * 应用观察者模式
- * @param {Object} state 
- * @param {Function} stateChanger 
+ * @param {Object} state
+ * @param {Function} stateChanger
  */
 function createStore(state, stateChanger) {
   const listeners = []
   const subscribe = listener => listeners.push(listener)
   const getState = () => state
   const dispatch = action => {
-    stateChanger(state, action)
+    // 覆盖原对象
+    state = stateChanger(state, action)
     listeners.forEach(listener => listener())
   }
   return {
@@ -33,40 +19,90 @@ function createStore(state, stateChanger) {
   }
 }
 
-function stateChanger(state, action) {
-  switch(action.type) {
-    case 'UPDATE_TITLE_TEXT':
-      state.title.text = action.text
-      break
-    case 'UPDATE_TITLE_COLOR':
-      state.title.color = action.color
-      break
-    default:
-      break
+/**
+ * 
+ * @param {Object} appState 
+ */
+function renderApp(newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return
+  console.log('render app...')
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
+}
+
+/**
+ * 
+ * @param {Object} title 
+ */
+function renderTitle(newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return
+  console.log('render title...')
+  const titleDOM = document.getElementById("title")
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = oldTitle.color
+}
+
+/**
+ * 
+ * @param {Object} content 
+ */
+function renderContent(newContent, oldContent = {}) {
+  if (newContent === oldContent) return
+  console.log('render content...')
+  const contentDOM = document.getElementById("content")
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = oldContent.color
+}
+
+const appState = {
+  title: {
+    text: "water make redux",
+    color: "red"
+  },
+  content: {
+    text: "water make redux",
+    color: "green"
   }
 }
 
-function renderTitle(title) {
-  const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
+function stateChanger(state, action) {
+  switch (action.type) {
+    case "UPDATE_TITLE_TEXT":
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case "UPDATE_TITLE_COLOR":
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state
+  }
 }
 
-function renderContent(content) {
-  const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
-}
-
-const store=  createStore(appState, stateChanger)
-store.subscribe(() => renderApp(store.getState()))
+const store = createStore(appState, stateChanger)
+let oldState = store.getState()
+// 监听数据变化
+store.subscribe(() => {
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+})
 
 renderApp(store.getState())
 store.dispatch({
-  type: 'UPDATE_TITLE_TEXT',
-  text: 'water is gay gay'
+  type: "UPDATE_TITLE_TEXT",
+  text: "water is gay gay"
 })
 store.dispatch({
-  type: 'UPDATE_TITLE_COLOR',
-  color: '#f00'
+  type: "UPDATE_TITLE_COLOR",
+  color: "#f00"
 })
